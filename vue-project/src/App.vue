627cea95ea3fdf818708b41ef83eb199633aceba
trybledteen <trybledteen@gmail.com>
2025-08-11 16:32:00 +0300

    install bootstrap + fortawesome, create components-TodoListApp

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,14 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <TodolistApp />
 </template>
 
-<style scoped></style>
+<script>
+import TodolistApp from './components/TodolistApp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodolistApp
+  }
+}
+</script>
